1546b20c1f90993b1cd90df1457efbeed7a9c69a
Lex Zar <gadkonb@ya.ru>
2021-11-29 10:44:00 +0300

    fixed

--- task_2/greeting_spec.rb
+++ task_2/greeting_spec.rb
@@ -1,6 +1,9 @@
 require 'rspec'
 
-def greeting(name,last_name,age)
+def greeting
+  name = gets.to_s
+  last_name = gets.to_s
+  age = gets.to_i
   if age < 18
     result = "Привет, #{name} #{last_name}.Тебе меньше 18 лет,но учится программировать никогда не рано."
     result
@@ -10,13 +13,14 @@ def greeting(name,last_name,age)
   end
 end
 
-
 RSpec.describe "Greeting methods" do
-  it "greeting under the age of 18" do
-    expect(greeting("lex","Luter",17)).to eq("Привет, lex Luter.Тебе меньше 18 лет,но учится программировать никогда не рано.")  
+  it "#greeting under the age of 18" do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return("lex","Luter",17)
+    expect(greeting).to eq("Привет, lex Luter.Тебе меньше 18 лет,но учится программировать никогда не рано.")  
   end
 
-  it "greetings over 18 years old" do
-    expect(greeting("lex","Luter", 18)).to eq("Привет, lex Luter.Самое время занятся делом!")  
+  it "#greetings over 18 years old" do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return("lex","Luter",19)
+    expect(greeting).to eq("Привет, lex Luter.Самое время занятся делом!")  
   end
 end
